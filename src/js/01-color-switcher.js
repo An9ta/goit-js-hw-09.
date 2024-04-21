fcc0a71d323deb7a4dffd3ec93b5ff94f6a20c07
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-21 19:56:32 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
+let timerId = null;
 
+function stopChangeColor() {
+  btnStop.setAttribute(`disabled`, '');
+  btnStart.removeAttribute(`disabled`);
+  clearInterval(timerId);
+}
+function changeBgc() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+function startChangeColor() {
+  btnStop.removeAttribute(`disabled`);
+  btnStart.setAttribute(`disabled`, '');
+  changeBgc();
+  timerId = setInterval(() => {
+    changeBgc();
+  }, 1000);
+}
+btnStop.setAttribute(`disabled`, '');
+btnStart.addEventListener('click', startChangeColor);
+btnStop.addEventListener('click', stopChangeColor);
